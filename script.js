522c98821c6b0a711c000bfe131954e15da7b30f
Sagnik Mandal <Sagnik@shorthillstech.com>
2022-06-30 13:24:17 +0530

    script.js update

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ Boolean, null, undefined, number, string
 */
 
 let a = true, b = null, c = undefined, d = 10, e = "abc";
-d="10.22";
-d=parseInt(d);
 console.log(a, typeof(a));
 console.log(b, typeof(b));
 console.log(c, typeof(c));
