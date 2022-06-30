91d80b8f9a4ff10237db74b667198cb33b9c46d0
Sagnik Mandal <Sagnik@shorthillstech.com>
2022-06-30 17:04:49 +0530

    functions added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*
 Datatypes in Javascript 
 Boolean, null, undefined, number, string
-*/
+
 
 let a = true, b = null, c = undefined, d = 10, e = "abc";
 console.log(a, typeof(a));
@@ -10,3 +10,18 @@ console.log(c, typeof(c));
 console.log(d, typeof(d));
 console.log(e, typeof(e));
 
+*/
+
+/* Functions
+
+ square  = function(number) {
+    return number * number;
+ }
+ console.log(square(5));
+
+ let square2 = (num) => (num*num);
+ console.log(square2(3));
+
+ */
+
+ 
