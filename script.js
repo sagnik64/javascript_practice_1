8126cd691ab3b0da045704fc3b51395876bae55a
Sagnik Mandal <Sagnik@shorthillstech.com>
2022-06-30 12:07:49 +0530

    initial files added

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+Datatypes in Javascript 
+Boolean, null, undefined, number, string
+*/
+
+let a = true, b = null, c = undefined, d = 10, e = "abc";
+d="10.22";
+d=parseInt(d);
+console.log(a, typeof(a));
+console.log(b, typeof(b));
+console.log(c, typeof(c));
+console.log(d, typeof(d));
+console.log(e, typeof(e));
+
